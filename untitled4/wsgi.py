983a6eae602e88c74e7f0982b3b0f9d6f71f84cf
Megaboss074 <mbilal1980@hotmail.com>
2018-06-06 00:44:12 +0500

    Latest

--- untitled4/wsgi.py
+++ untitled4/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_cloud.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "untitled4.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
