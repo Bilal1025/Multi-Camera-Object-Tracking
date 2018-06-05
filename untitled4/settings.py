fcf1004c943364f8b63f88dc63ead48f35d34b1d
Megaboss074 <mbilal1980@hotmail.com>
2018-06-06 01:33:18 +0500

    Latest

--- untitled4/settings.py
+++ untitled4/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,5 +127,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # ]
 
 STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
-
-DATABASES['default'] = dj_database_url.config()
